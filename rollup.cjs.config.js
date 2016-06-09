9722d0c235fcaf375641fac0cd3defc5bced21e4
Brian Donovan <me@brian-donovan.com>
2016-06-08 17:09:50 -0700

    Use the `targets` option to write to multiple target files.

--- rollup.cjs.config.js
+++
@@ -1,6 +0,0 @@
-import config from './rollup.config';
-
-config.format = 'cjs';
-config.dest = 'dist/rollup-plugin-multi-entry.cjs.js';
-
-export default config;
--- rollup.config.js
+++ rollup.config.js
@@ -2,5 +2,17 @@ import babel from 'rollup-plugin-babel';
 
 export default {
   entry: 'index.js',
-  plugins: [babel()]
+  plugins: [babel()],
+  targets: [
+    {
+      format: 'cjs',
+      exports: 'named',
+      dest: 'dist/rollup-plugin-multi-entry.cjs.js'
+    },
+    {
+      format: 'es6',
+      exports: 'named',
+      dest: 'dist/rollup-plugin-multi-entry.es6.js'
+    }
+  ]
 };
