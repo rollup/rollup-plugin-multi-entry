f37f865dc512d73f546afcc8ae950450bafe52d4
Brian Donovan <me@brian-donovan.com>
2018-01-07 10:49:36 -0800

    style: use prettier to format JavaScript

--- test/test.js
+++ test/test.js
@@ -4,13 +4,23 @@ import { rollup } from 'rollup';
 
 function includes(string, substring) {
   if (string.indexOf(substring) === -1) {
-    ok(false, `expected ${JSON.stringify(string)} to include ${JSON.stringify(substring)}`);
+    ok(
+      false,
+      `expected ${JSON.stringify(string)} to include ${JSON.stringify(
+        substring
+      )}`
+    );
   }
 }
 
 function doesNotInclude(string, substring) {
   if (string.indexOf(substring) !== -1) {
-    ok(false, `expected ${JSON.stringify(string)} not to include ${JSON.stringify(substring)}`);
+    ok(
+      false,
+      `expected ${JSON.stringify(string)} not to include ${JSON.stringify(
+        substring
+      )}`
+    );
   }
 }
 
@@ -21,67 +31,72 @@ function makeBundle(entries) {
 describe('rollup-plugin-multi-entry', () => {
   it('takes a single file as input', () => {
     return makeBundle('test/fixtures/0.js').then(bundle => {
-      return bundle.generate({ format: 'cjs' }).then(({code}) => {
+      return bundle.generate({ format: 'cjs' }).then(({ code }) => {
         includes(code, 'exports.zero = zero;');
       });
-    })
+    });
   });
 
   it('takes an array of files as input', () => {
-    return makeBundle(['test/fixtures/0.js', 'test/fixtures/1.js']).then(bundle => {
-      return bundle.generate({ format: 'cjs' }).then(({code}) => {
-        includes(code, 'exports.zero = zero;');
-        includes(code, 'exports.one = one;');
-      });
-    })
+    return makeBundle(['test/fixtures/0.js', 'test/fixtures/1.js']).then(
+      bundle => {
+        return bundle.generate({ format: 'cjs' }).then(({ code }) => {
+          includes(code, 'exports.zero = zero;');
+          includes(code, 'exports.one = one;');
+        });
+      }
+    );
   });
 
   it('allows an empty array as input', () => {
     return makeBundle([]).then(bundle => {
-      return bundle.generate({ format: 'cjs' }).then(({code}) => {
+      return bundle.generate({ format: 'cjs' }).then(({ code }) => {
         doesNotInclude(code, 'exports');
       });
-    })
+    });
   });
 
   it('takes a glob as input', () => {
     return makeBundle('test/fixtures/{0,1}.js').then(bundle => {
-      return bundle.generate({ format: 'cjs' }).then(({code}) => {
+      return bundle.generate({ format: 'cjs' }).then(({ code }) => {
         includes(code, 'exports.zero = zero;');
         includes(code, 'exports.one = one;');
       });
-    })
+    });
   });
 
   it('takes an array of globs as input', () => {
-    return makeBundle(['test/fixtures/{0,}.js', 'test/fixtures/{1,}.js']).then(bundle => {
-      return bundle.generate({ format: 'cjs' }).then(({code}) => {
-        includes(code, 'exports.zero = zero;');
-        includes(code, 'exports.one = one;');
-      });
-    })
+    return makeBundle(['test/fixtures/{0,}.js', 'test/fixtures/{1,}.js']).then(
+      bundle => {
+        return bundle.generate({ format: 'cjs' }).then(({ code }) => {
+          includes(code, 'exports.zero = zero;');
+          includes(code, 'exports.one = one;');
+        });
+      }
+    );
   });
 
   it('takes an {include,exclude} object as input', () => {
-    return makeBundle(
-      { include: ['test/fixtures/*.js'], exclude: ['test/fixtures/1.js'] }
-    ).then(bundle => {
-      return bundle.generate({ format: 'cjs' }).then(({code}) => {
+    return makeBundle({
+      include: ['test/fixtures/*.js'],
+      exclude: ['test/fixtures/1.js']
+    }).then(bundle => {
+      return bundle.generate({ format: 'cjs' }).then(({ code }) => {
         includes(code, 'exports.zero = zero;');
         doesNotInclude(code, 'exports.one = one;');
       });
-    })
+    });
   });
 
   it('allows to prevent exporting', () => {
-    return makeBundle(
-      { include: ['test/fixtures/*.js'], exports: false }
-    ).then(bundle => {
-      return bundle.generate({ format: 'cjs' }).then(({code}) => {
-        includes(code, `console.log('Hello, 2');`)
-        doesNotInclude(code, 'zero');
-        doesNotInclude(code, 'one');
-      });
-    })
+    return makeBundle({ include: ['test/fixtures/*.js'], exports: false }).then(
+      bundle => {
+        return bundle.generate({ format: 'cjs' }).then(({ code }) => {
+          includes(code, `console.log('Hello, 2');`);
+          doesNotInclude(code, 'zero');
+          doesNotInclude(code, 'one');
+        });
+      }
+    );
   });
 });
